65848e9c7017ba185247ae06e7996c5ce2e3a959
JBodyan <31380945+JBodyan@users.noreply.github.com>
2018-09-22 16:57:26 +0300

    fail

--- app/src/main/java/weather_informer_application/bk/wia/adapters/WeatherAdapter.java
+++ app/src/main/java/weather_informer_application/bk/wia/adapters/WeatherAdapter.java
@@ -1,24 +1,4 @@
 package weather_informer_application.bk.wia.adapters;
 
-
-import java.util.List;
-
-import weather_informer_application.bk.wia.entities.Weather;
-import weather_informer_application.bk.wia.entities.WeatherList;
-
 public class WeatherAdapter {
-    Weather weather = null;
-
-    public void setWeather(Weather weather){
-        this.weather = weather;
-    }
-
-    public Weather getWeather() {
-        return weather;
-    }
-
-    public List<WeatherList> getWeatherList(){
-        return weather.getList();
-    }
-
 }
---
+++ app/src/main/java/weather_informer_application/bk/wia/entities/other/DatesHelper.java
@@ -0,0 +1,4 @@
+package weather_informer_application.bk.wia.entities.other;
+
+public class DatesHelper {
+}
